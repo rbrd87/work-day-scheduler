351a43cb3cfac74956035803d39a2e996fa95f10
rbrd87 <rochellembradley@gmail.com>
2023-01-20 19:37:32 +0000

    Added the colour changing to the timeBlockHrs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,22 @@
 // Moment JS for the date in the jumbrotron
-var datetime = null,
-    date = null;
+$("#currentDay").text(moment().format("dddd Do MMM YYYY"));
 
-var update = function () {
-  date = moment(new Date())
-  datetime.html(date.format("dddd Do MMM YYYY, HH:mm:ss"));
-};
+function timeBlockState() {
+  // Gets the current hour and data index of the hours timeblock store them in variables
+  let currentHour = moment().hour();
+  const timeBlockHrs = $(".time-block");
 
-
-$(document).ready(function(){
-  datetime = $('#currentDay')
-  update();
-  setInterval(update, 1000 * 60)
-});
+  for (let i = 0; i < timeBlockHrs.length; i++) {
+    const currentTimeBlock = $(timeBlockHrs[i]).attr("data-index");
+    // If the currentHour matches the currentTimeBlock - it adds a class to change the timeblock colour to match 'Present'
+    // If the currentHour is less than the currentTimeBlock - it adds a class to change the timeblock colour to match 'Future'
+    // If the currentHour is greater than the currentTimeBlock - it adds a class to change the timeblock colour to match 'Past'
+    if (currentHour == currentTimeBlock) {
+        $(timeBlockHrs[i]).addClass("present");
+    } else if (currentHour < currentTimeBlock) {
+      $(timeBlockHrs[i]).addClass("future");
+    } else if (currentHour > currentTimeBlock) {
+      $(timeBlockHrs[i]).addClass("past");
+    }
+  }
+}
